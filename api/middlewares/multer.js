f59e8991faecf26f30148174c467ac0acea505f5
FaiqaN27 <faiqanadeem6725@gmail.com>
2025-12-14 20:17:48 +0500

    change folder name for deployment

--- api/middlewares/multer.js
+++ api/middlewares/multer.js
@@ -1,8 +1,9 @@
 import multer from "multer";
 
+const uploadPath = "/tmp";
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    return cb(null, "./uploads");
+    return cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
     return cb(null, `${Date.now()}-${file.originalname}`);
